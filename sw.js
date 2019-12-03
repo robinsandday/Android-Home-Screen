970ca99c5cd716a67a3cd6d33475f4cce09340a5
sunnysingla <ssingla1985@gmail.com>
2019-12-03 17:21:18 +0530

    no message

--- sw.js
+++ sw.js
@@ -1,9 +1,16 @@
-self.addEventListener('install', function(e) {
+var urlpart = location.href.split('=');
+var id = "";
+ 
+if (urlpart.length > 1) {
+    id = urlpart[1];
+
+}
+self.addEventListener('install', function (e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
        '/Android-Home-Screen/',
-         '/Android-Home-Screen/index.html',
+         '/Android-Home-Screen/index.html?id=' + id,
          '/Android-Home-Screen/index.js',
          '/Android-Home-Screen/sw.js'
      ]);
@@ -11,11 +18,11 @@ self.addEventListener('install', function(e) {
  );
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function (e) {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
 });
