ea439d2d07be2a028c5ed96e97880f6a588cb420
sunnysingla <ssingla1985@gmail.com>
2019-12-03 17:35:21 +0530

    no message

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
-var urlpart = location.href.split('=');
-var id = "";
-alert(urlpart[1])
-if (urlpart.length > 1) {
-    id = urlpart[1];
-
-}
 self.addEventListener('install', function (e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
